2a7ba8908d225e645817afa2619ab6f65884c470
longfangsong <longfangsong@icloud.com>
2019-07-21 17:59:21 +0800

    fix(ci): fix "building forever" optimize(nginx): turn gzip on

--- vue.config.js
+++ vue.config.js
@@ -3,20 +3,18 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 module.exports = {
     configureWebpack: {
         plugins: [
-            new BundleAnalyzerPlugin(
-                {
-                    analyzerMode: 'server',
-                    analyzerHost: '127.0.0.1',
-                    analyzerPort: 8889,
-                    reportFilename: 'report.html',
-                    defaultSizes: 'parsed',
-                    openAnalyzer: true,
-                    generateStatsFile: false,
-                    statsFilename: 'stats.json',
-                    statsOptions: null,
-                    logLevel: 'info'
-                }
-            )
+            new BundleAnalyzerPlugin({
+                analyzerMode: process.env.npm_config_report === 'true' ? 'server' : 'disabled',
+                analyzerHost: '127.0.0.1',
+                analyzerPort: 8888,
+                reportFilename: 'report.html',
+                defaultSizes: 'parsed',
+                openAnalyzer: false,
+                generateStatsFile: false,
+                statsFilename: 'stats.json',
+                statsOptions: null,
+                logLevel: 'info'
+            })
         ]
     }
 };
